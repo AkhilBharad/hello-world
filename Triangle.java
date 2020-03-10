c8f4de50d9eed21eb6487e40943be7a59a3d3d93
AkhilBharad <bharadwaj16akhil@gmail.com>
2020-03-09 23:46:51 -0700

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,19 @@
+ 
+import java.util.Scanner;
+class AreaOfTriangle 
+{
+   public static void main(String args[]) 
+    {   
+       
+      Scanner s= new Scanner(System.in);
+        
+         
+         double b;
+ 
+          double h;
+ 
+                  //Area = (width*height)/2
+      double area=(10*2)/2;
+      System.out.println("Area of Triangle is: " + area);      
+   }
+}
